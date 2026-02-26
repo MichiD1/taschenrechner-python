ed755ce56bb1d784f9729060adb02ac6521a5b81
MichiD1 <michid1810@gmail.com>
2026-02-26 01:04:20 +0100

    Update taschenrechner.py

--- taschenrechner.py
+++ taschenrechner.py
@@ -1,24 +1,23 @@
 def eingabe_zahl(text):
-    while True:                 # Schleife läuft unendlich break beendet die Schleife
+    while True:                 
         try:
-            return float(input(text))       # float() = wandelt Text (z.B. 3.14) in eine Zahl um
+            return float(input(text))       
         except ValueError:
             print("Bitte nur Zahlen eingeben!")
 
 def eingabe_operator():
     while True: 
         op = input("Welche Operation möchtest du? (+, -, *, %, /) oder 'q' zum Beenden: ")
-        if op in ["+", "-", "*", "/", "%", "q"]:                # Prüft, ob der Wert in der Liste enthalten ist 
+        if op in ["+", "-", "*", "/", "%", "q"]:                
             return op
         else:
             print("Ungültiger Operator! Bitte +, -, *, %, / oder q eingeben.")
 
-# def = definiert eine Funktion
-# Alles, was eingerückt darunter steht, gehört zu dieser Funktion
+
 def berechne(zahl1, zahl2, operator):
     if operator == "+":
-        return zahl1 + zahl2        # return = gibt das Ergebnis der Funktion zurück
-    elif operator == "-":           # Der Code außerhalb der Funktion kann dieses Ergebnis dann benutzen
+        return zahl1 + zahl2        
+    elif operator == "-":           
         return zahl1 - zahl2
     elif operator == "%":
         return zahl1 * zahl2 / 100
@@ -29,13 +28,11 @@ def berechne(zahl1, zahl2, operator):
             return zahl1 / zahl2
         else:
             return "Fehler: Division durch 0"
-# if = wenn Bedingung erfüllt ist
-# elif = sonst wenn eine andere Bedingung erfüllt ist
-#else = sonst
 
-#HauptProgramm 
-name = input("Wie heißt du? ")                              # input() = fragt den Benutzer nach einer Eingabe 
-print(f"Willkommen, {name}, zu deinem Taschenrechner!")     # Das Ergebnis ist immer ein Text (String)
+
+
+name = input("Wie heißt du? ")                               
+print(f"Willkommen, {name}, zu deinem Taschenrechner!")     
 
 while True:
     zahl1 = eingabe_zahl("Gib die erste Zahl ein: ")
@@ -45,7 +42,8 @@ while True:
     if operator == "q":
         print("Taschenrechner beendet. Auf Wiedersehen!")
         break
-# break = verlässt die aktuelle Schleife sofort
+
 
     ergebnis = berechne(zahl1, zahl2, operator)
     print(f"{name}, dein Ergebnis ist: {ergebnis}\n")
+
